1830cb0adf6a54aae7cf1a6174ceb6c7d4be2bdf
mdNazmulHasan <najmul.hasan10@gmail.com>
2015-09-03 10:50:25 +0600

    just some little tweak, nothing much important

--- app/src/main/java/com/nerdcastle/mdnazmulhasan/inventoryapp/DashboardActivity.java
+++ app/src/main/java/com/nerdcastle/mdnazmulhasan/inventoryapp/DashboardActivity.java
@@ -1,7 +1,5 @@
 package com.nerdcastle.mdnazmulhasan.inventoryapp;
 
-import android.annotation.TargetApi;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -14,7 +12,6 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.JsonObjectRequest;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -24,29 +21,47 @@ import java.util.ArrayList;
 
 public class DashboardActivity extends AppCompatActivity {
     ArrayList<String>orderList=new ArrayList<>();
+    ListView listView;
+    int dataUpto;
+    boolean more=false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-        View header = getLayoutInflater().inflate(R.layout.header, null);
-        final ListView listView = (ListView) findViewById(R.id.listView);
-        listView.addHeaderView(header);
+        //View header = getLayoutInflater().inflate(R.layout.header, null);
+         listView = (ListView) findViewById(R.id.listView);
+        //listView.addHeaderView(header);
+        dataRequest();
+
+    }
+
+    public void dataRequest() {
+
+
         String url="http://dotnet.nerdcastlebd.com/Bazar/api/orders";
         JsonArrayRequest request=new JsonArrayRequest(Request.Method.GET, url, new Response.Listener<JSONArray>() {
             @Override
             public void onResponse(JSONArray response) {
+                if(more){
+                    dataUpto=response.length();
+                }
+                else{
+                    dataUpto=5;
+                }
+
                 try {
-                    for(int i=0;i<response.length();i++){
+                    for(int i=0;i<dataUpto;i++){
                         JSONObject singleData= response.getJSONObject(i);
                         String orderNo=singleData.getString("OrderNo");
                         String date=singleData.getString("Date");
                         String status=singleData.getString("Status");
-                        String singleOrder=orderNo+" "+date+" "+status;
+                        String singleOrder=orderNo+"      "+date+"      "+status;
                         orderList.add(singleOrder);
                     }
                     ArrayAdapter<String> itemsAdapter =
-                            new ArrayAdapter<>(getApplicationContext(),R.layout.each_row_dashboard,R.id.orderTextView,orderList);
+                            new ArrayAdapter<>(getApplicationContext(), R.layout.each_row_dashboard,R.id.orderTextView,orderList);
                     listView.setAdapter(itemsAdapter);
 
 
@@ -64,6 +79,10 @@ public class DashboardActivity extends AppCompatActivity {
         });
         request.setRetryPolicy(new DefaultRetryPolicy(DefaultRetryPolicy.DEFAULT_TIMEOUT_MS, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
         AppController.getInstance().addToRequestQueue(request);
+    }
+    public void moreData(View view){
+        more=true;
+        dataRequest();
 
     }
 
